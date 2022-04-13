b4fd94da406ff01162463abbb0789ec21f9f4b99
shabanelmogy <92805780+shabanelmogy@users.noreply.github.com>
2022-04-13 17:41:01 +0200

    Update

--- UpdateNewClinic/Forms/Frm_Reservation.vb
+++ UpdateNewClinic/Forms/Frm_Reservation.vb
@@ -15,101 +15,7 @@ Public Class Frm_Reservation
 
 #End Region
 
-#Region "Datagridview"
-
-    Sub FormatDgv_Search()
-        Dgv_Search.Columns(0).Width = 100
-        Dgv_Search.Columns(1).Width = 290
-        Dgv_Search.Columns(2).Width = 170
-    End Sub
-
-    Sub FormatDgv_Visits()
-        Dgv_Visits.Columns(0).Width = 100
-        Dgv_Visits.Columns(1).Width = 300
-        Dgv_Visits.Columns(2).Width = 120
-        Dgv_Visits.Columns(3).Width = 100
-        Dgv_Visits.Columns(4).Width = 100
-        Dgv_Visits.Columns(3).HeaderText = "ReserveType"
-        Dgv_Visits.Columns(0).HeaderText = "PatientNum"
-    End Sub
-
-    Private Sub Dgv_Search_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles Dgv_Search.CellClick
-        Txt_PatientName.Text = Dgv_Search.CurrentRow.Cells(1).Value.ToString
-        Txt_Num.Text = Dgv_Search.CurrentRow.Cells(0).Value.ToString
-        check = False
-    End Sub
-
-    Private Sub Dgv_Visits_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles Dgv_Visits.CellClick
-        Txt_Num.Text = Dgv_Visits.CurrentRow.Cells(0).Value
-        Txt_PatientName.Text = Dgv_Visits.CurrentRow.Cells(1).Value
-        Dtp_ReserveDate.Text = Dgv_Visits.CurrentRow.Cells(2).Value
-        Cbo_ReserveType.Text = Dgv_Visits.CurrentRow.Cells(3).Value
-        Txt_VisitCost.Text = Dgv_Visits.CurrentRow.Cells(4).Value
-        check = True
-    End Sub
-
-#End Region
-
-#Region "Buttons"
-
-    Private Sub Btn_NewPatient_Click(sender As Object, e As EventArgs) Handles Btn_NewPatient.Click
-        Dim frm As New Frm_NewClient
-        frm.StartPosition = FormStartPosition.CenterScreen
-        frm.Txt_PatientNum.Text = GetMaxId("PatientNum", "PatientsDetail") + 1
-        frm.Show()
-    End Sub
-
-    Private Sub Btn_SelectAll_Click(sender As Object, e As EventArgs) Handles Btn_SelectAll.Click
-        Frm_Reservation_Load(Nothing, Nothing)
-    End Sub
-
-    Private Sub Btn_SaveNewPatient_Click(sender As Object, e As EventArgs) Handles Btn_SaveNewPatient.Click
-        If Not String.IsNullOrWhiteSpace(Txt_Num.Text) And Not String.IsNullOrWhiteSpace(Cbo_ReserveType.Text) Then
-            If check = False Then
-
-                Insert_Reservation()
-            Else
-                Update_Reservation()
-            End If
-
-            Txt_Num.Text = ""
-            Txt_PatientName.Text = ""
-            Cbo_ReserveType.Text = ""
-            Txt_VisitCost.Text = ""
-            Txt_SearchValue.Select()
-
-            GetAllReservation()
-        End If
-
-    End Sub
-
-    Private Sub Btn_SortDesc_Click(sender As Object, e As EventArgs) Handles Btn_SortDesc.Click
-        Dgv_Search.Sort(Dgv_Search.Columns(Cbo_SortAndSearch.Text), System.ComponentModel.ListSortDirection.Descending)
-    End Sub
-
-    Private Sub Btn_SortAsec_Click(sender As Object, e As EventArgs) Handles Btn_SortAsec.Click
-        Dgv_Search.Sort(Dgv_Search.Columns(Cbo_SortAndSearch.Text), System.ComponentModel.ListSortDirection.Ascending)
-    End Sub
-
-#End Region
-
-
-
-
-
-    Private Sub Frm_Reservation_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Try
-            FillCmb(Cbo_ReserveType, "VisitsTypes", "VisitKind", "Num")
-            Cbo_SortAndSearch.SelectedIndex = 0
-            GetAllPatient("Select PatientNum,PatientName,PhoneNumber From PatientsDetail")
-            GetAllReservation()
-            TextBoxDepndOnCombobox(Txt_VisitCost, Cbo_ReserveType)
-            Dtp_ReserveDate.Value = Date.Now.ToString("dd-MM-yyyy")
-            check = False
-        Catch ex As Exception
-        MsgBox(ex.Message, MsgBoxStyle.Information)
-        End Try
-    End Sub
+#Region "Sub Procedures"
 
     Sub GetAllPatient(Query As String)
         Try
@@ -203,8 +109,135 @@ Public Class Frm_Reservation
         con.Close()
     End Sub
 
+#End Region
+
+#Region "Datagridview"
+
+    Sub FormatDgv_Search()
+        Dgv_Search.Columns(0).Width = 100
+        Dgv_Search.Columns(1).Width = 290
+        Dgv_Search.Columns(2).Width = 170
+    End Sub
+
+    Sub FormatDgv_Visits()
+        Dgv_Visits.Columns(0).Width = 100
+        Dgv_Visits.Columns(1).Width = 300
+        Dgv_Visits.Columns(2).Width = 120
+        Dgv_Visits.Columns(3).Width = 100
+        Dgv_Visits.Columns(4).Width = 100
+        Dgv_Visits.Columns(3).HeaderText = "ReserveType"
+        Dgv_Visits.Columns(0).HeaderText = "PatientNum"
+    End Sub
+
+    Private Sub Dgv_Search_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles Dgv_Search.CellClick
+        Txt_PatientName.Text = Dgv_Search.CurrentRow.Cells(1).Value.ToString
+        Txt_Num.Text = Dgv_Search.CurrentRow.Cells(0).Value.ToString
+        check = False
+    End Sub
+
+    Private Sub Dgv_Visits_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles Dgv_Visits.CellClick
+        Txt_Num.Text = Dgv_Visits.CurrentRow.Cells(0).Value
+        Txt_PatientName.Text = Dgv_Visits.CurrentRow.Cells(1).Value
+        Dtp_ReserveDate.Text = Dgv_Visits.CurrentRow.Cells(2).Value
+        Cbo_ReserveType.Text = Dgv_Visits.CurrentRow.Cells(3).Value
+        Txt_VisitCost.Text = Dgv_Visits.CurrentRow.Cells(4).Value
+        check = True
+    End Sub
+
+#End Region
+
+#Region "Events"
+
+#Region "Buttons"
+
+    Private Sub Btn_NewPatient_Click(sender As Object, e As EventArgs) Handles Btn_NewPatient.Click
+        Dim frm As New Frm_NewClient
+        frm.StartPosition = FormStartPosition.CenterScreen
+        frm.Txt_PatientNum.Text = GetMaxId("PatientNum", "PatientsDetail") + 1
+        frm.Show()
+    End Sub
+
+    Private Sub Btn_SelectAll_Click(sender As Object, e As EventArgs) Handles Btn_SelectAll.Click
+        Frm_Reservation_Load(Nothing, Nothing)
+    End Sub
+
+    Private Sub Btn_SaveNewPatient_Click(sender As Object, e As EventArgs) Handles Btn_SaveNewPatient.Click
+        If Not String.IsNullOrWhiteSpace(Txt_Num.Text) And Not String.IsNullOrWhiteSpace(Cbo_ReserveType.Text) Then
+            If check = False Then
+
+                Insert_Reservation()
+            Else
+                Update_Reservation()
+            End If
+
+            Txt_Num.Text = ""
+            Txt_PatientName.Text = ""
+            Cbo_ReserveType.Text = ""
+            Txt_VisitCost.Text = ""
+            Txt_SearchValue.Select()
+
+            GetAllReservation()
+        End If
+
+    End Sub
+
+    Private Sub Btn_SortDesc_Click(sender As Object, e As EventArgs) Handles Btn_SortDesc.Click
+        Dgv_Search.Sort(Dgv_Search.Columns(Cbo_SortAndSearch.Text), System.ComponentModel.ListSortDirection.Descending)
+    End Sub
+
+    Private Sub Btn_SortAsec_Click(sender As Object, e As EventArgs) Handles Btn_SortAsec.Click
+        Dgv_Search.Sort(Dgv_Search.Columns(Cbo_SortAndSearch.Text), System.ComponentModel.ListSortDirection.Ascending)
+    End Sub
+
+#End Region
+
+#Region "TextBox"
+
+    Private Sub Txt_SearchValue_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Txt_SearchValue.KeyPress
+        If Cbo_SortAndSearch.Text = "PatientName" And Char.IsControl(e.KeyChar) = False And Char.IsLetter(e.KeyChar) = False Then
+            e.Handled = True
+
+        ElseIf Cbo_SortAndSearch.Text = "Phone" And Char.IsControl(e.KeyChar) = False And Char.IsDigit(e.KeyChar) = False Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Sub Txt_SearchValue_KeyDown(sender As Object, e As KeyEventArgs) Handles Txt_SearchValue.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            dv = Dt_Search.DefaultView
+            If Cbo_SortAndSearch.Text = "PatientName" Then
+                dv.RowFilter = "PatientName like '" & Txt_SearchValue.Text & "%'"
+            ElseIf Cbo_SortAndSearch.Text = "PhoneNumber" Then
+                dv.RowFilter = "PhoneNumber = '" & Txt_SearchValue.Text & "'"
+            End If
+        End If
+    End Sub
+
+#End Region
+
+#Region "ComboBox"
+
     Private Sub Cbo_SortAndSearch_TextChanged(sender As Object, e As EventArgs) Handles Cbo_SortAndSearch.TextChanged
         Txt_SearchValue.Text = ""
+    End Sub
+
+#End Region
+
+#Region "Forms"
+
+    Private Sub Frm_Reservation_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+            FillCmb(Cbo_ReserveType, "VisitsTypes", "VisitKind", "Num")
+            Cbo_SortAndSearch.SelectedIndex = 0
+            GetAllPatient("Select PatientNum,PatientName,PhoneNumber From PatientsDetail")
+            GetAllReservation()
+            TextBoxDepndOnCombobox(Txt_VisitCost, Cbo_ReserveType)
+            Dtp_ReserveDate.Value = Date.Now.ToString("dd-MM-yyyy")
+            check = False
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Information)
+        End Try
     End Sub
 
     Private Sub Frm_NewClient_KeyUp(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
@@ -216,43 +249,8 @@ Public Class Frm_Reservation
         End Select
     End Sub
 
-    Private Sub Txt_SearchValue_KeyDown(sender As Object, e As KeyEventArgs) Handles Txt_SearchValue.KeyDown
-        If e.KeyCode = Keys.Enter Then
-            dv = Dt_Search.DefaultView
-            If Cbo_SortAndSearch.Text = "PatientName" Then
-                dv.RowFilter = "PatientName like '" & Txt_SearchValue.Text & "%'"
-            ElseIf Cbo_SortAndSearch.Text = "PhoneNumber" Then
-            dv.RowFilter = "PhoneNumber = '" & Txt_SearchValue.Text & "'"
-            End If
-            End If
-    End Sub
+#End Region
 
-
-
-
-
-
-
-
-
-
-
-    Private Sub Txt_SearchValue_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Txt_SearchValue.KeyPress
-        If Cbo_SortAndSearch.Text = "PatientName" And Char.IsControl(e.KeyChar) = False And Char.IsLetter(e.KeyChar) = False Then
-            e.Handled = True
-        ElseIf Cbo_SortAndSearch.Text = "Phone" And Char.IsControl(e.KeyChar) = False And Char.IsDigit(e.KeyChar) = False Then
-            e.Handled = True
-        End If
-    End Sub
-
-
-
-    Private Sub Cbo_ReserveType_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles Cbo_ReserveType.SelectionChangeCommitted
-        If Cbo_ReserveType.Text <> "" Then
-            TextBoxDepndOnCombobox(Txt_VisitCost, Cbo_ReserveType)
-        End If
-    End Sub
-
-
+#End Region
 
 End Class
